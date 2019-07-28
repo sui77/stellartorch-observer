1d19047aa81161695db13dbe92f617b4d0f60ddf
Suat Özgür <s.oezguer@gmail.com>
2019-07-28 12:55:07 +0200

    rename config.js

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
     keybase: {
         username: 'suibot',
-        paperkey: 'door ...',
-        channel: {name: 'stellartorch', membersType: 'team', topicName: 'general'}
+        paperkey: 'door invite position rookie garment visual crush vehicle artefact fabric region transfer patch',
+        channel: {name: 'sui_zabbix', membersType: 'team', topicName: 'test'}
     },
     stellar: {
-        horizonUrl: 'https://horizon.stellar.org',
-        accountId: 'GB2P5P4EOYFJW2DSAHJQO7TXTBZZSH4CAS6M5TYM5EJRH7VQTQL7T2KC',
+        horizonUrl: 'https://horizon-testnet.stellar.org',
+        accountId: 'GAE74VY3VFK7QVYFUXEKDPY7ZKSK3J4XU3H67OPE2UXGCHKBL5SPQK5M',
     },
     opencagedata: {
-        apiKey: '...',
+        apiKey: '9ea3afa6eca24bd381d6100dfd6e5ba5',
     }
 }
